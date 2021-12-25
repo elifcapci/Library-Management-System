2ece7bc331b810ea5196ce7cdf0ed403dd929b4b
elifcapci <elifcapci1@gmail.com>
2021-12-25 22:14:30 +0300

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -1,1 +1,12 @@
+public class Book {
 
+    private int ID;
+    private String Name;
+    private String PublicationDate;
+    private String Publisher;
+    private int NumberOfPages;
+    private String language;
+    private String authorID;
+    private String ISBNNumber;
+    private String category;
+}
